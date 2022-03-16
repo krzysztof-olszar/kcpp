fc9b4d72643b3dd04a528a8456d1d99184e88452
Krzysztof Olszar <95043903+krzysztof-olszar@users.noreply.github.com>
2022-03-16 12:30:30 +0100

    Add files via upload

---
+++ 2022.03.16/ZadMainExample.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void dodawanie(){
+	cout<<"2+2=4"<<endl;
+}
+
+void duzaLista(){
+	cout<<"Cos"<<endl;
+}
+
+
+int main(int argc, char *argv[]){
+	
+	return 0;
+}
